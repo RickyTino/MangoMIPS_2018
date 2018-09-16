ca7f134db1dfedf1763c0b3a9c7ee7495b2b92fa
RickyTino <294970540@qq.com>
2018-09-16 17:01:56 +0800

    [20180916] Set up version without IP core.

--- CPU/DIV.v
+++ CPU/DIV.v
@@ -3,77 +3,55 @@
 module DIV 
 (
 	input  wire            clk, rst,
+	
 	input  wire            divsigned, start, abandon,
 	input  wire [`DataBus] opr1, opr2,
+
 	output reg             ready,
 	output reg  [`DblData] res
 );
 	
-	wire        aresetn = !(rst || abandon);
-    reg         opr_valid;
-    reg  [31:0] divisor,    dividend;
-    wire        sres_valid, ures_valid;
-    wire [63:0] sres,       ures;
-	
-	wire        res_valid = divsigned ? sres_valid : ures_valid;
-	wire [63:0] tempres   = divsigned ? sres       : ures;
-	
-	Divider_Signed divS(
-		.aclk                    (clk),
-		.aresetn                 (aresetn),
-		.s_axis_divisor_tvalid   (opr_valid),
-		.s_axis_divisor_tdata    (divisor),
-		.s_axis_dividend_tvalid  (opr_valid),
-		.s_axis_dividend_tdata   (dividend),
-		.m_axis_dout_tvalid      (sres_valid),
-		.m_axis_dout_tdata       (sres)
-    );     
-	
-	Divider_Unsigned divU(
-		.aclk                    (clk),
-		.aresetn                 (aresetn),
-		.s_axis_divisor_tvalid   (opr_valid),
-		.s_axis_divisor_tdata    (divisor),
-		.s_axis_dividend_tvalid  (opr_valid),
-		.s_axis_dividend_tdata   (dividend),
-		.m_axis_dout_tvalid      (ures_valid),
-		.m_axis_dout_tdata       (ures)
-    );
-	
-	wire [31:0] topr1, topr2;
+	wire [32:0] temp;
+	reg  [ 5:0] cnt;
+	reg  [64:0] dividend;
 	reg  [ 1:0] state;
+	reg  [31:0] divisor;
+	wire [31:0] topr1, topr2;
 	
-	parameter DIVIDLE = 2'b00;
-	parameter DIVON   = 2'b01;
-	parameter DIVBYZ  = 2'b10;
+	parameter DIVFREE = 2'b00;
+	parameter DIVBYZ  = 2'b01;
+	parameter DIVON   = 2'b10;
 	parameter DIVEND  = 2'b11;
 	
-	//assign topr1 = divsigned && opr1[31] ? ~opr1 + 1 : opr1;
-	//assign topr2 = divsigned && opr2[31] ? ~opr2 + 1 : opr2;
+	assign temp = {1'b0, dividend[63:32]} - {1'b0, divisor};
+	
+	assign topr1 = divsigned && opr1[31] ? ~opr1 + 1 : opr1;
+	assign topr2 = divsigned && opr2[31] ? ~opr2 + 1 : opr2;
+	
 	
 	always @(posedge clk, posedge rst) begin
 		if(rst) begin
-			state      <= DIVIDLE;
-			ready      <= `false;
-			res        <= `ZeroDWord;
-			dividend   <= `ZeroWord;
-			divisor    <= `ZeroWord;
-			opr_valid <= `false;
+			state    <= DIVFREE;
+			ready    <= `false;
+			res      <= `ZeroDWord;
+			dividend <= 65'b0;
+			divisor  <= `ZeroWord;
+			cnt      <= 6'd0;
 		end
 		else begin
-			opr_valid <= `false;
-			
 			case(state)
-				DIVIDLE: begin
+				DIVFREE: begin
 					if(start && !abandon) begin
 						if(opr2 == `ZeroWord) begin
 							state <= DIVBYZ;
 						end
 						else begin
-							state      <= DIVON;
-							dividend   <= opr1;
-							divisor    <= opr2;
-							opr_valid  <= `true;
+							state    <= DIVON;
+							cnt      <= 6'd0;
+							//topr1    <= divsigned && opr1[31] ? ~opr1 + 1 : opr1;
+							//topr2    <= divsigned && opr2[31] ? ~opr2 + 1 : opr2;
+							dividend <= {31'b0, topr1, 1'b0};
+							divisor  <= topr2;
 						end
 					end
 					else begin
@@ -82,29 +60,39 @@ module DIV
 					end
 				end
 				
-				DIVON: begin
-					if(!abandon) begin
-						if(res_valid) begin
-							res[63:32] <= tempres[31: 0];
-							res[31: 0] <= tempres[63:32];
-							ready <= `true;
-							state <= DIVEND;
-						end
-					end
-					else begin
-						state <= DIVIDLE;
-					end
-				end
-				
 				DIVBYZ: begin
-					res      <= `ZeroDWord;
-					ready    <= `true;
+					dividend <= `ZeroDWord;
 					state    <= DIVEND;
 				end
 				
+				DIVON: begin
+					if(!abandon) begin
+						if(cnt != 6'd32) begin
+							if(temp[32] == 1'b1)
+								dividend <= {dividend[63:0], 1'b0};
+							else
+								dividend <= {temp[31:0], dividend[31:0], 1'b1};
+							cnt <= cnt + 1;
+						end
+						else begin
+							if(divsigned && (opr1[31] ^ opr2[31]))
+								dividend[31:0] <= ~dividend[31:0] + 1;
+							if(divsigned && (opr1[31] ^ dividend[64]))
+								dividend[64:33] <= ~dividend[64:33] + 1;
+							state <= DIVEND;
+							cnt   <= 6'd0;
+						end
+					end
+					else begin
+						state <= DIVFREE;
+					end
+				end
+				
 				DIVEND: begin
+					res <= {dividend[64:33], dividend[31:0]};
+					ready <= `true;
 					if(!start) begin
-						state <= DIVIDLE;
+						state <= DIVFREE;
 						ready <= `false;
 						res   <= `ZeroDWord;
 					end
